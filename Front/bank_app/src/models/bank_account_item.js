19b28de9421faefd73aca77c4cda78e21843be5c
Andrew Trubitsin <truba24032016@mail.ru>
2021-06-26 16:44:25 +0500

    Изменить название файла, задать объекты как константу

--- Front/bank_app/src/models/bank_account_item.js
+++ Front/bank_app/src/models/bank_account_item.js
@@ -6,8 +6,9 @@ import {useBankPage} from "../blocks/mainField/bank_page/bank_page_provider";
 function BankAccountItem({account, index}){
     const bankPage = useBankPage()
     return (
+
         <li onClick={() => bankPage.changeAcc(account)}>
-            {index} {account.GetTitle} {account.GetValue} {account.GetCurrency}
+            {index} {account.Title} {account.Value} {account.Currency}
         </li>
     );
 }
